1314e96cacf6fd9266a2aec8505b23d7a21ca405
yura702007 <yura702007@gmail.com>
2024-03-25 06:50:06 +0300

    create class model manager

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
 
     class Status(models.TextChoices):
