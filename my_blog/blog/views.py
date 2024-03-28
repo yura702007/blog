9eb328268e96879aadc052b1adcca1cd410e0116
yura702007 <82802568+yura702007@users.noreply.github.com>
2024-03-28 12:31:06 +0300

    create function view post_detail()

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from django.http import Http404
 from .models import Post
 
 
 def post_list(request):
     posts = Post.published.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, id_):
+    try:
+        post = Post.published.get(id=id_)
+    except Post.DoesNotExist:
+        raise Http404
+
+    return render(request, 'blog/post/list-detail.html', {'post': post})
 
 # Create your views here.
