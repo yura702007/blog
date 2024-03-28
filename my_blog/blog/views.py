1868f8ce15294774b73f876fedadd5aa453ab47b
yura702007 <82802568+yura702007@users.noreply.github.com>
2024-03-28 16:01:05 +0300

    bug fix

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -7,8 +7,8 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, id_):
-    post = get_object_or_404(Post, id=id_, status=Post.status.PUBLISHED)
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
 
     return render(request, 'blog/post/detail.html', {'post': post})
 
