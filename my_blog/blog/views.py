e470428a077adb3f73a8899bfe263b48759179db
yura702007 <82802568+yura702007@users.noreply.github.com>
2024-04-02 14:56:19 +0300

    add function 'send_mail()' to function view 'post_share()'

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from .forms import EmailPostForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
 
 
 class PostListView(ListView):
@@ -49,16 +50,21 @@ def post_detail(request, year, month, day, post):
 def post_share(request, post_id):
     #  Извлечь пост по идентификатору id
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
     if request.method == 'POST':
         #  Извлечь пост по идентификатору id
         form = EmailPostForm(request.POST)
         if form.is_valid():
             #  Поля формы успешно прошли валидацию
             cd = form.cleaned_data
-            #  ...отправить электронное письмо
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'yura702007@gmail.com', cd['to'])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
 
 # Create your views here.
