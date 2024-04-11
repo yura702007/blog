bd70dc5bcbf3453945be91d95951294687067709
yura702007 <82802568+yura702007@users.noreply.github.com>
2024-04-11 15:19:44 +0300

    add sorting post by tags in function "post_detail()"

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -6,6 +6,7 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 
 class PostListView(ListView):
@@ -54,7 +55,16 @@ def post_detail(request, year, month, day, post):
     # Форма для комментирования пользователями
     form = CommentForm()
 
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
+    #  Список схожих постов
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+    return render(request, 'blog/post/detail.html', {
+        'post': post,
+        'comments': comments,
+        'form': form,
+        'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
@@ -91,6 +101,5 @@ def post_comment(request, post_id):
         # Сохранить комментарий в базе данных
         comment.save()
     return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
-
 
 # Create your views here.
