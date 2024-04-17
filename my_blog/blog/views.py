431f4677e27f80cc39be4b846263a0439d0848fd
yura702007 <yura702007@gmail.com>
2024-04-17 05:20:40 +0300

    bug fix

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -113,11 +113,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
-    return (request,
-            'blog/post/search.html',
-            {'form': form,
-             'query': query,
-             'results': results})
+            results = Post.published.annotate(search=SearchVector('title', 'body'), ).filter(search=query)
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
 
 # Create your views here.
