e80d3a90a97a1b45fb474b7640a120e996ccf58c
yura702007 <yura702007@gmail.com>
2024-04-17 03:08:49 +0300

    change database sqlite >> postgresql

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -82,8 +82,10 @@ WSGI_APPLICATION = 'my_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': 'password',
     }
 }
 
